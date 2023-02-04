ef5cdbb8b29b4a906423f92a47e8327e7ce7b447
Yuvraj Anand <yuvrajanand@Yuvrajs-MacBook-Air-2.local>
2023-02-05 00:37:06 +0530

    initial commit

---
+++ index01.js
@@ -0,0 +1,9 @@
+const submitEl = document.getElementById("btn")
+
+
+submitEl.addEventListener("click", function(){
+    // let onsubmit= `<a href="file:///Users/yuvrajanand/Documents/CODING/Projects/Project003/index11.html">link</a>`
+    // submitEl.innerHTML = onsubmit
+    window.open("file:///Users/yuvrajanand/Documents/CODING/Projects/Project003/index11.html")
+    // console.log(window.open(onsubmit))
+})
